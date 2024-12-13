354e113ca390aabc0ebc2bb55490b891c105bac6
archer <78126270+tagaryen@users.noreply.github.com>
2024-12-13 15:48:05 +0800

    Update ArcherSequence.java

--- sdk/archer-rpc-java/src/main/java/com/archer/rpcjson/ArcherSequence.java
+++ sdk/archer-rpc-java/src/main/java/com/archer/rpcjson/ArcherSequence.java
@@ -13,10 +13,9 @@ public class ArcherSequence {
         this.size = size / P + 1;
         map = new N[this.size];
     }
-
-    //32bytes input
-    public synchronized void add(byte[] item, ArcherCallback obj) {
-        long p = 0;
+    
+    long calP (byte[] item) {
+    	long p = 0;
         for(int i = 0; i < C; i++) {
             long l = (long) item[i];
             if(l < 0) {
@@ -27,38 +26,35 @@ public class ArcherSequence {
         if(p < 0) {
             p = -p;
         }
+        return p;
+    }
+    //32bytes input
+    public synchronized void add(byte[] item, ArcherCallback obj) {
+        long p = calP(item);
         int r = (int) (p % size);
         if(map[r] == null) {
             map[r] = new N(item);
             map[r].obj = obj;
         } else {
-            N cur = new N(item);
-            map[r].obj = obj;
-            map[r].next = cur;
-            cur.last = map[r];
-            map[r] = cur;
+        	N cur = map[r];
+        	while(cur.next != null) {
+        		cur = cur.next;
+        	}
+        	cur.next = new N(item);
+        	cur.next.obj = obj;
         }
     }
 
-    public ArcherCallback find(byte[] bs) {
-        long p = 0;
-        for(int i = 0; i < C; i++) {
-            long l = (long) bs[i];
-            if(l < 0) {
-                l += 256;
-            }
-            p |= (l << ((C - i) << 3));
-        }
-        if(p < 0) {
-            p = -p;
-        }
-        int m = map.length;
-        int r = (int) (p % m);
-        N cur = map[r];
+    public synchronized ArcherCallback find(byte[] bs) {
+    	long p = calP(bs);
+        int m = map.length, r = (int) (p % m);
+    	N prev = null;
+    	N cur = map[r];
+        boolean ok = true;
         while(cur != null) {
             byte[] d = cur.data;
             if(d.length == bs.length) {
-                boolean ok = true;
+            	ok = true;
                 for(int i = 0; i < bs.length; i++) {
                     if(d[i] != bs[i]) {
                         ok = false;
@@ -66,22 +62,16 @@ public class ArcherSequence {
                     }
                 }
                 if(ok) {
-                	N prev = cur.last;
-                	N next = cur.next;
                 	if(cur == map[r]) {
-                		map[r] = next;
-                	}
-                	if(prev != null) {
-                		
-                		prev.next = next;
-                	}
-                	if(next != null) {
-                		next.last = prev;
+                		map[r] = cur.next;
+                	} else {
+                		prev.next = cur.next;
                 	}
                     return cur.obj;
                 }
             }
-            cur = cur.last;
+            prev = cur;
+            cur = cur.next;
         }
         return null;
     }
@@ -89,7 +79,6 @@ public class ArcherSequence {
     
 	static class N {
 		byte[] data;
-		N last;
 		N next;
 		
 		ArcherCallback obj;
