aa05adb8f9b1b129c1e2144d8324679889da24b7
xuyi <xu_yi@cestc.cn>
2026-04-02 20:33:19 +0800

    add build bin and reame

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # tiny_file_server
 A tiny file server
+
+## API 
+
+### Public API
+- 1. {endpoint}/fs/open-api/v1/upload  
+     POST  
+     body=FormData  
+- 2. {endpoint}/fs/open-api/v1/view?filename=xxx  
+     GET  
+- 3. {endpoint}/fs/open-api/v1/download?filename=xxx  
+     GET  
+- 4. {endpoint}/fs/open-api/v1/json?key=xxx  
+     GET  
+
+     POST  
+     body={"xxx":"xxx"}  
+
+
+### Private API
+- 1. {endpoint}/fs/api/v1/upload?filename=xxx  
+     POST  
+     body=FormData  
+- 2. {endpoint}/fs/api/v1/view?filename=xxx  
+     GET  
+- 3. {endpoint}/fs/api/v1/download?filename=xxx  
+     GET  
+- 4. {endpoint}/fs/api/v1/json?key=xxx  
+     GET  
+
+     POST  
+     body={"xxx":"xxx"}  
--- request.js
+++ request.js
@@ -1,36 +1,78 @@
 const http = require("http");
 const fs = require('fs');
 
-let data = fs.readFileSync("./LICENSE", 'utf-8')
+function uploadFile() {
+    let data = fs.readFileSync("./README.md", 'utf-8');
+    const options = {
+        hostname: 'localhost',
+        port: 9617,
+        path: '/fs/open-api/v1/upload?filename=readme.md',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/markdown',
+            'Content-Length': data.length,
+        },
+    };
 
-const options = {
-    hostname: 'localhost',
-    port: 10001,
-    path: '/fs/open-api/v1/upload?filename=LISENCE',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'text/markdown',
-        'Content-Length': data.length,
-    },
-};
-
-//http://localhost:10001/fs/open-api/v1/view?filename=LISENCE
-const req = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-        console.log(`BODY: ${chunk}`);
+    //visite http://localhost:9617/fs/open-api/v1/view?filename=readme.md
+    const req = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
     });
-    res.on('end', () => {
-        console.log('No more data in response.');
+
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
     });
-});
 
-req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-});
+    // Write data to request body
+    req.write(data);
+    req.end();
+}
 
-// Write data to request body
-req.write(data);
-req.end();
+function saveJson() {
+
+    const json = '{"xy":"haoshuai"}';
+    const options = {
+        hostname: 'localhost',
+        port: 9617,
+        path: '/fs/open-api/v1/json?key=xy',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': json.length,
+        },
+    };
+
+    //http://localhost:9617/fs/open-api/v1/json?key=xy
+    const req = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+
+
+    // Write data to request body
+    req.write(json);
+    req.end();
+}
+
+
+uploadFile();
+saveJson();
